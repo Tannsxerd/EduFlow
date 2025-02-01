5768b466a38d5dd8d3030aaaed0a533615b39ac2
Tannsxerd <tansxerd1231@gmail.com>
2025-02-01 20:54:52 +0700

    login_page_unittest

--- board/tests.py
+++ board/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase 
+from django.http import HttpRequest 
+from board.views import login_view
 
 # Create your tests here.
+class page_test(TestCase):
+    def test_login_page(self):
+        request = HttpRequest()  
+        response = login_view(request)  
+        html = response.content.decode("utf8")  
+        self.assertIn("<title>EduFlow</title>", html)

236199c78f8c122f02a315a1a314c39d665df3e8
Tannsxerd <tansxerd1231@gmail.com>
2025-02-02 15:28:40 +0700

    unit_test_user

--- board/tests.py
+++ board/tests.py
@@ -1,9 +1,17 @@
 from django.test import TestCase 
 from django.http import HttpRequest 
 from board.views import login_view
+from board.models import KanbanUser
 
 # Create your tests here.
-class page_test(TestCase):
+class Login_test(TestCase):
+    def test_user(self):
+        #create test user
+        user = KanbanUser.objects.create(username = "testuser" , password = "testpass1234")
+        self.assertEqual(user.username,"testuser" )
+        self.assertEqual(user.password , "testpass1234")
+        self.assertEqual(str(user), "testuser") 
+
     def test_login_page(self):
         request = HttpRequest()  
         response = login_view(request)  
